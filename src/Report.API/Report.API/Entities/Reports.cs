cc62487ca0a9a1fdea61336331338a1e0b4dc725
Asya İlktürk <asya.ilkturk@gmail.com>
2021-01-17 18:43:22 +0300

    Added report api

---
+++ src/Report.API/Report.API/Entities/Reports.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Report.API.Entities
+{
+    public class Reports
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ReportId { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        //Status false for "Prepairing" and true for "Done"
+        public bool Status { get; set; }
+    }
+}
